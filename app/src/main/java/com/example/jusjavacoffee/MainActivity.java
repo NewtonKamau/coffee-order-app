eb21eed7784556b9c26f866fd25bc9c40810cfa3
newnyc <kamaunewton78@gmail.com>
2019-10-09 14:21:54 +0300

    added increment and decrement methods and displayPrice methods

--- app/src/main/java/com/example/jusjavacoffee/MainActivity.java
+++ app/src/main/java/com/example/jusjavacoffee/MainActivity.java
@@ -1,23 +1,22 @@
 package com.example.jusjavacoffee;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
-
 import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
-
+   int numberOfCoffees = 2;
+   int quantity = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
     public void submitOrder(View view) {
-        display(1);
-        displayPrice(2 * 5);
+        display(quantity);
+        displayPrice(quantity* 5);
     }
     private void display(int number) {
         TextView quantityTextView =  findViewById(R.id.quantity_text_view);
@@ -26,5 +25,13 @@ public class MainActivity extends AppCompatActivity {
     private void displayPrice(int number) {
         TextView priceTextView = findViewById(R.id.price_text_view);
         priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+
+    }
+    public void increment(View view){
+      display(quantity = quantity + 1);
+    }
+    public void decrement(View view){
+        display(quantity = quantity - 1);
+
     }
 }
