588249cc01e9c4b4b61c80191493a83813b745c4
newnyc <kamaunewton78@gmail.com>
2019-10-11 10:09:38 +0300

    modified price ()

--- app/src/main/java/com/example/jusjavacoffee/MainActivity.java
+++ app/src/main/java/com/example/jusjavacoffee/MainActivity.java
@@ -7,16 +7,21 @@ import android.widget.TextView;
 import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
-   int numberOfCoffees = 2;
    int quantity = 0;
+   String priceMessage = "free";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
     public void submitOrder(View view) {
-        display(quantity);
-        displayPrice(quantity* 5);
+       int price = quantity * 5;
+        String priceMessage = "Total $" + price;
+        priceMessage = priceMessage + "\n Thank you";
+        displayMessage(priceMessage);
+
+//        display(quantity);
+//        displayPrice(quantity* 5);
     }
     private void display(int number) {
         TextView quantityTextView =  findViewById(R.id.quantity_text_view);
@@ -33,5 +38,9 @@ public class MainActivity extends AppCompatActivity {
     public void decrement(View view){
         display(quantity = quantity - 1);
 
+    }
+    private void displayMessage(String message) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(message);
     }
 }
